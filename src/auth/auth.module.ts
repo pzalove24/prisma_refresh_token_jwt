689efd2840f1b37a2fef4dcc9025c384961b3c94
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2024-01-24 22:26:09 +0700

    init

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { AtStrategy, RtStrategy } from './strategies';
+import {JwtModule} from '@nestjs/jwt' 
+
+
+@Module({
+  imports: [],
+  providers: [AuthService, AtStrategy, RtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
